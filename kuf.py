83039866147eb53542d05a06475295551dde6ed5
debnoob <68480653+debnoob@users.noreply.github.com>
2021-02-05 15:44:33 -0500

    Update kuf.py

--- kuf.py
+++ kuf.py
@@ -1,7 +1,7 @@
 import discord
 import os 
 client = discord.Client()
-
+token="NzMzNjA5MTcyOTY4NjY5MzA0.XxFoyA.4_qp5np1kR7CZh3DgehxCZVBt9M"
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -15,4 +15,4 @@ async def on_message(message):
         await message.channel.send(file=discord.File('Screenshot from 2021-02-04 08-51-42.png'))
                                                   
 
-client.run(os.environ['token'])
+client.run(token)
